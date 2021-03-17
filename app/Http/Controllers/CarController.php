c8573768167091ccfd58a5d6365cf7982b194357
Fabio Petrone <fa610movic90@gmail.com>
2021-03-17 18:06:51 +0100

    condizione per id fuori range

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -8,43 +8,46 @@ class CarController extends Controller
 {
   public function index($id = null)
   {$cars = Car::all();
-      // return view('home', compact('cars'));
-      if($id==null){
+    // return view('home', compact('cars'));
+    if($id==null){
       return view('home',['cars'=>$cars]);
+    }
+    else{
+      if($id <= count($cars)){
+        $car = Car::find($id);
+        return view('product',['car'=>$car]);
+      }else{
+        abort(404);
+      }
+
+    };
+
   }
-  else{
-    $car = Car::find($id);
-    return view('product',['car'=>$car]);
 
+  public function index2($id = null)
+  {$cars = Car::all();
+    // return view('home', compact('cars'));
+    if($id==null){
+      return view('home',['cars'=>$cars]);
+    }
+    else{
+      $car = collect($cars)->where('id', $id)->first();
+      return view('product',['car'=>$car]);
+    };
+  }
 
-};
-
- }
-
- public function index2($id = null)
- {$cars = Car::all();
-     // return view('home', compact('cars'));
-     if($id==null){
-     return view('home',['cars'=>$cars]);
- }
- else{
-   $car = collect($cars)->where('id', $id)->first();
-       return view('product',['car'=>$car]);
-};
- }
-
-//  public function index3($id = null)
-//  {$cars = Car::all();
-//      // return view('home', compact('cars'));
-//      if($id==null){
-//      return view('home',['cars'=>$cars]);
-//  }
-//  else{
-//    $collection = collect($cars);
-//    $car = $collection->filter(function ($value, $id) {
-//     return collect($value)['id'] == $id;
-// });
-// return view('product',['car'=>$car]);
-// };
-//  }
+  //  public function index3($id = null)
+  //  {$cars = Car::all();
+  //      // return view('home', compact('cars'));
+  //      if($id==null){
+  //      return view('home',['cars'=>$cars]);
+  //  }
+  //  else{
+  //    $collection = collect($cars);
+  //    $car = $collection->filter(function ($value, $id) {
+  //     return collect($value)['id'] == $id;
+  // });
+  // return view('product',['car'=>$car]);
+  // };
+  //  }
 }
